3bebfda77acb1a0eea4a273f0c3362c7e220f79b
Alexander Lapikov <racman996@gmail.com>
2022-03-15 23:55:52 +0300

    16fs

--- library.js
+++ library.js
@@ -1,3 +1,12 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 function isLatinLetter(letter) {
     if (letter.length >= 2) {
         console.error("' ".concat(letter, " '") + ' consists of two or more letters! Use only one-letter string as a parameter.');
@@ -60,4 +69,102 @@ function getFactorial(input) {
 function subtractArr(input, arrOfExtra) {
     var result = input.filter(function (el) { return !arrOfExtra.includes(el); });
     return result;
+}
+function getScreenRatio() {
+    var screenRatio = (document.documentElement.clientHeight / document.documentElement.clientWidth).toFixed(3);
+    return screenRatio;
+}
+function kmsToMls(kms) {
+    var miles = kms / 1.609;
+    return miles;
+}
+function mlsToKms(miles) {
+    var kms = miles * 1.609;
+    return kms;
+}
+function celsToFahr(celsius) {
+    var fahrenheit = ((celsius * 1.8) + 32).toFixed(1);
+    return fahrenheit;
+}
+function fahrToCels(fahrenheit) {
+    var celsius = ((fahrenheit - 32) / 1.8).toFixed(1);
+    return celsius;
+}
+function bubbleSort(input) {
+    var result = __spreadArray([], input, true);
+    for (var q = 0; q < result.length; q++) {
+        for (var r = 1; r < result.length; r++) {
+            if (result[r] < result[r - 1]) {
+                var buff = result[r];
+                result[r] = result[r - 1];
+                result[r - 1] = buff;
+            }
+        }
+    }
+    return result;
+}
+function shakerSort(input) {
+    var result = __spreadArray([], input, true);
+    var indexLeft = 0;
+    var indexRight = result.length - 1;
+    while (indexLeft <= indexRight) {
+        for (var q = indexRight; q > indexLeft; q--) {
+            if (result[q - 1] > result[q]) {
+                var buff = result[q];
+                result[q] = result[q - 1];
+                result[q - 1] = buff;
+            }
+        }
+        indexLeft++;
+        for (var r = indexLeft; r < indexRight; r++) {
+            if (result[r] > result[r + 1]) {
+                var buff = result[r];
+                result[r] = result[r + 1];
+                result[r + 1] = buff;
+            }
+        }
+        indexRight--;
+    }
+    return result;
+}
+function selectionSort(input) {
+    var result = __spreadArray([], input, true);
+    var startIndex = 0;
+    while (startIndex < result.length - 1) {
+        var minIndex = startIndex;
+        var numMin = result[minIndex];
+        for (var q = startIndex; q < result.length; q++) {
+            if (result[q] < numMin) {
+                numMin = result[q];
+                minIndex = q;
+            }
+        }
+        var buff = result[startIndex];
+        result[startIndex] = numMin;
+        result[minIndex] = buff;
+        startIndex++;
+    }
+    return result;
+}
+function mergeSort(input) {
+    function merge(arr1, arr2) {
+        var result = [];
+        var q = 0;
+        var r = 0;
+        while (q < arr1.length && r < arr2.length) {
+            result.push((arr1[q] < arr2[r]) ?
+                arr1[q++] : arr2[r++]);
+        }
+        return __spreadArray(__spreadArray(__spreadArray([], result, true), arr1.slice(q), true), arr2.slice(r), true);
+    }
+    if (input.length === 1) {
+        return input;
+    }
+    else {
+        var result = __spreadArray([], input, true);
+        var middle = Math.floor(result.length / 2);
+        var arrLeft = result.slice(0, middle);
+        var arrRight = result.slice(middle);
+        return merge(mergeSort(arrLeft), mergeSort(arrRight));
+    }
 }
--- library.ts
+++ library.ts
@@ -65,6 +65,115 @@ function getFactorial(input: number) {
 }
 
 function subtractArr(input: (string | number)[], arrOfExtra: (string | number)[]) {    
-    let result = input.filter(el => !arrOfExtra.includes(el))
-    return result
+    let result = input.filter(el => !arrOfExtra.includes(el));
+    return result;
+}
+
+function getScreenRatio() {
+    const screenRatio = (document.documentElement.clientHeight  / document.documentElement.clientWidth).toFixed(3);
+    return screenRatio;
+}
+
+function kmsToMls(kms: number) {
+    const miles = kms / 1.609;
+    return miles;
+}
+
+function mlsToKms(miles: number) {
+    const kms = miles * 1.609;
+    return kms;
+}
+
+function celsToFahr(celsius: number) {
+    const fahrenheit = ((celsius * 1.8) + 32).toFixed(1);
+    return fahrenheit;
+}
+
+function fahrToCels(fahrenheit: number) {
+    const celsius = ((fahrenheit - 32) / 1.8).toFixed(1);
+    return celsius;
+}
+
+function bubbleSort(input: number[]) {
+    const result = [...input];
+    for (let q = 0; q < result.length; q++) {
+        for (let r = 1; r < result.length; r++) {
+            if (result[r] < result[r - 1]) {
+                const buff = result[r];
+                result[r] = result[r - 1];
+                result[r - 1] = buff;
+            }
+        }
+    }
+    return result;
+}
+
+function shakerSort(input: number[]) {
+    const result = [...input];
+    let indexLeft = 0;
+    let indexRight = result.length - 1;
+    while (indexLeft <= indexRight) {
+        for (let q = indexRight; q > indexLeft; q--) {
+            if (result[q - 1] > result[q]) {
+                const buff = result[q];
+                result[q] = result[q - 1];
+                result[q - 1] = buff
+            }
+        }
+        indexLeft++;
+        for (let r = indexLeft; r < indexRight; r++) {
+            if (result[r] > result[r + 1]) {
+                const buff = result[r];
+                result[r] = result[r + 1];
+                result[r + 1] = buff;
+            }
+        }
+        indexRight--;
+    }
+    return result;
+}
+
+function selectionSort(input: number[]) {
+    const result = [...input];
+    let startIndex = 0;
+    while (startIndex < result.length - 1) {
+        let minIndex = startIndex;
+        let numMin = result[minIndex];
+        for (let q = startIndex; q < result.length; q++) {
+            if (result[q] < numMin) {
+                numMin = result[q];
+                minIndex = q;
+            }
+        }
+        const buff = result[startIndex];
+        result[startIndex] = numMin;
+        result[minIndex] = buff;
+        startIndex++;
+    }
+    return result;
+}
+
+function mergeSort(input: number[]) {
+    function merge(arr1, arr2) {
+        const result = [];
+        let q = 0;
+        let r = 0;
+        while(q < arr1.length && r < arr2.length) {
+            result.push(
+                (arr1[q] < arr2[r]) ?
+                arr1[q++] : arr2[r++]
+            )
+        }
+        return [...result, ...arr1.slice(q), ...arr2.slice(r)]
+    }
+
+    if (input.length === 1) {
+        return input
+    } else {
+        const result = [...input];
+        const middle = Math.floor(result.length / 2);
+        const arrLeft = result.slice(0, middle);
+        const arrRight = result.slice(middle);
+        return merge(mergeSort(arrLeft), mergeSort(arrRight));
+    }
 }
